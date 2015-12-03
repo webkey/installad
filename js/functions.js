a5ff1f99d2148619c72d778cef6be71ef811ad34
Serhii <serhiy.ilchenko@gmail.com>
2015-12-03 04:02:32 +0300

    20151202 3

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,69 @@
-/* placeholder */
+/*placeholder*/
 function placeholderInit(){
 	$('[placeholder]').placeholder();
 }
-/* placeholder end */
+/*placeholder end*/
+
+/*equalHeight*/
+function equalHeightInit(){
+	var bonusesList = $('.vantages-list');
+	bonusesList.find('.vantage-img div').equalHeight({
+		amount: 5,
+		useParent: true,
+		parent: bonusesList,
+		resize: true
+	});
+}
+/*equalHeight end*/
+
+/* yandexMap */
+function yandexMap(){
+	var mapYandex = $('.location-map');
+	if ( mapYandex.length ) {
+		mapYandex.each(function(index){
+			var obj = $(this);
+			var objIndex = index + 1;
+			var className = obj.attr('class');
+			obj.attr('id', 'map-'+objIndex);
+			var id = obj.attr('id');
+			var latitude = obj.data('latitude');
+			var longitude = obj.data('longitude');
+			var zoom = obj.data('zoom');
+			ymaps.ready(function () {
+				// Create map
+				var myMap = new ymaps.Map(id, {
+					center: [latitude, longitude],
+					zoom: zoom,
+					scrollwheel: false
+				});
+				var myPlacemark = new ymaps.Placemark([latitude, longitude],{
+					//hintContent: 'Name',
+					//help_hint: 'Name',
+					//balloonContent: '<div>Name</div>',
+					balloonContentHeader: "Балун метки",
+					balloonContentBody: "Содержимое <em>балуна</em> метки",
+					balloonContentFooter: "Подвал",
+					hintContent: "Хинт метки"
+				}, {
+					iconImageHref: './img/pin.png',
+					iconImageSize: [60, 72],
+					iconImageOffset: [-19, -53]
+				});
+				// Add buttons and placemarks
+				myMap.controls.add('zoomControl', {top: '10px', left:'10px', height: '50px'});
+				myMap.geoObjects.add(myPlacemark);
+			});
+		});
+	}
+}
+/* yandexMap */
 
 /** ready/load/resize document **/
 
 $(document).ready(function(){
 	placeholderInit();
+	yandexMap();
+});
+$(window).load(function () {
+	equalHeightInit();
 });
--- js/widgets.js
+++ js/widgets.js
@@ -1,9 +1,25 @@
 /**
  --- WIDGETS CONTAINS ---
  1. Placeholder
+ 2. equalHeight
  */
 
 /**--- 1. Placeholder ---*/
 /*Placeholder. https://github.com/mathiasbynens/jquery-placeholder, http://mths.be/placeholder v2.0.8*/
 (function(m,g,d){function r(b){var a={},c=/^jQuery\d+$/;d.each(b.attributes,function(b,d){d.specified&&!c.test(d.name)&&(a[d.name]=d.value)});return a}function h(b,a){var c=d(this);if(this.value==c.attr("placeholder")&&c.hasClass("placeholder"))if(c.data("placeholder-password")){c=c.hide().next().show().attr("id",c.removeAttr("id").data("placeholder-id"));if(!0===b)return c[0].value=a;c.focus()}else this.value="",c.removeClass("placeholder"),this==n()&&this.select()}function l(){var b,a=d(this),c= this.id;if(""==this.value){if("password"==this.type){if(!a.data("placeholder-textinput")){try{b=a.clone().attr({type:"text"})}catch(e){b=d("<input>").attr(d.extend(r(this),{type:"text"}))}b.removeAttr("name").data({"placeholder-password":a,"placeholder-id":c}).bind("focus.placeholder",h);a.data({"placeholder-textinput":b,"placeholder-id":c}).before(b)}a=a.removeAttr("id").hide().prev().attr("id",c).show()}a.addClass("placeholder");a[0].value=a.attr("placeholder")}else a.removeClass("placeholder")} function n(){try{return g.activeElement}catch(b){}}var f="[object OperaMini]"==Object.prototype.toString.call(m.operamini),k="placeholder"in g.createElement("input")&&!f,f="placeholder"in g.createElement("textarea")&&!f,e=d.fn,p=d.valHooks,q=d.propHooks;k&&f?(e=e.placeholder=function(){return this},e.input=e.textarea=!0):(e=e.placeholder=function(){this.filter((k?"textarea":":input")+"[placeholder]").not(".placeholder").bind({"focus.placeholder":h,"blur.placeholder":l}).data("placeholder-enabled", !0).trigger("blur.placeholder");return this},e.input=k,e.textarea=f,e={get:function(b){var a=d(b),c=a.data("placeholder-password");return c?c[0].value:a.data("placeholder-enabled")&&a.hasClass("placeholder")?"":b.value},set:function(b,a){var c=d(b),e=c.data("placeholder-password");if(e)return e[0].value=a;if(!c.data("placeholder-enabled"))return b.value=a;""==a?(b.value=a,b!=n()&&l.call(b)):c.hasClass("placeholder")?h.call(b,!0,a)||(b.value=a):b.value=a;return c}},k||(p.input=e,q.value=e),f||(p.textarea= e, q.value=e),d(function(){d(g).delegate("form","submit.placeholder",function(){var b=d(".placeholder",this).each(h);setTimeout(function(){b.each(l)},10)})}),d(m).bind("beforeunload.placeholder",function(){d(".placeholder").each(function(){this.value=""})}))})(this,document,jQuery);
 /**--- 1. Placeholder end ---*/
+
+/**--- 2. equalHeight ---*/
+/*
+ Плагин equalHeight v1.0.0
+ Описание: Предназначен для посчета большей высоты рядом стоящих элементов и присвоения этой высоты.
+ Компания: DemingPro.com
+ Разработчик: Александр Ляшенко
+
+ Вызов плагина: $(object).equalHeight();
+ Опции: amount - кол-во блоков между которыми будет считаться максимальная высота (default: 2)
+
+ Compressed code of equalHeight v1.0.0:
+ */
+(function(e){e.fn.equalHeight=function(t){var t=e.extend({amount:2,useParent:false,parent:null,resize:false},t);var n=e(this);n.removeAttr("style");if(t.useParent==true){if(t.parent==null){var r=e(this).parent().outerWidth()}else{var r=t.parent.outerWidth()}var i=e(this).outerWidth(true);t.amount=parseInt(r/i);newAmount=t.amount}var s=this;if(t.resize==true&&t.useParent==true){e(window).resize(function(){n.removeAttr("style");if(t.useParent==true){if(t.parent==null){var r=s.parent().outerWidth()}else{var r=t.parent.outerWidth()}var i=s.outerWidth(true);t.amount=parseInt(r/i)}return s.each(function(r){if(r%t.amount===0){var i=r+1;var s=e(this);var o=s.index();var u=[];var a=[s];for(var f=1;f<t.amount;f++){var l=e(n[r+f]);a.push(l)}for(var c=0;c<t.amount;c++){u.push(a[c].height())}var h=Math.max.apply(Math,u);e(a).each(function(t){var n=e(this);n.css("height",h)})}})})}return this.each(function(r){if(r%t.amount===0){var i=r+1;var s=e(this);var o=s.index();var u=[];var a=[s];for(var f=1;f<t.amount;f++){var l=e(n[r+f]);a.push(l)}for(var c=0;c<t.amount;c++){u.push(a[c].height())}var h=Math.max.apply(Math,u);e(a).each(function(t){var n=e(this);n.css("height",h)})}})}})(jQuery);
+/**--- 2. equalHeight end ---*/
